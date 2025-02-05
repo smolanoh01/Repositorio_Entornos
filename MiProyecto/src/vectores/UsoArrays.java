192d578afa593fa7488f0f5fb3a0a37de8b66b96
smolanoh01 <smolanoh01@educarex.es>
2025-02-05 10:52:11 +0100

    Modificación realizada por Sergio Molano Hisado

--- MiProyecto/src/vectores/UsoArrays.java
+++ MiProyecto/src/vectores/UsoArrays.java
@@ -5,6 +5,8 @@ public class UsoArrays {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de metodo generado automaticamente
+		
+		//Este procedimiento se ha modificado por Sergio Molano Hisado
 
 		int opcion=Integer.parseInt(JOptionPane.showInputDialog("Dame una opcion (1-2)"));
 
